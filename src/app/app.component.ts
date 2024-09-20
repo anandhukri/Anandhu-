21fbc741a8281f4de6b61bb7605fd3f3ca12bebd
Anandhukrishnan Pulikkal Sreenivasan <w0852083@myscc.ca>
2024-09-20 12:25:03 -0700

    Assignment 2 finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Movies} from "./Shared/Models/movies";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AnandhukrishnanAngularLearn';
-  name : string ='MY NAME IS ANANDHU';
-  age : string ='20';
-}
 
+  title = 'Assignment2';
+  movie1: Movies [] = [ {name:"Barroz",director:"Mohenlal",productionCompany : "Ashirvad cinemas", country : "India", year: 2024, alreadyWatched: true },
+    {name:"ARM",director:"Jithin lal",productionCompany:"Magic Frames",country:"",year:2024, alreadyWatched: false},
+    {name:"GOATLIFE",director:"Blessy",productionCompany:"PJ filims",country:"",year:2024, alreadyWatched: true},
+    {name:"Oppenheimer",director:"Christopher Nolan",productionCompany:"Christopher Nolan",country:"America",year:2024, alreadyWatched: true},
+    {name:"Avatar",director:"James Cameron",productionCompany:"warner bros",country:"America",year:2023, alreadyWatched: true},
+    {name:"Spiderman",director:"Sam Raimi",productionCompany:"marvel",country:"America",year:2024, alreadyWatched: false}
+
+  ]
+
+}
