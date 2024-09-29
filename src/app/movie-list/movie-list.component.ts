38481bb49b3074669cbc17b39f1f34351c1fc077
Anandhukrishnan Pulikkal Sreenivasan <w0852083@myscc.ca>
2024-09-28 20:22:57 -0700

    create new component

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Movies} from "../Shared/Models/movies";
 
 @Component({
   selector: 'app-movie-list',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './movie-list.component.css'
 })
 export class MovieListComponent {
+  movie1: Movies [] = [ {name:"Barroz",director:"Mohenlal",productionCompany : "Ashirvad cinemas", country : "India", year: 2024, alreadyWatched: true },
+    {name:"ARM",director:"Jithin lal",productionCompany:"Magic Frames",country:"",year:2024, alreadyWatched: false},
+    {name:"GOATLIFE",director:"Blessy",productionCompany:"PJ filims",country:"",year:2024, alreadyWatched: true},
+    {name:"Oppenheimer",director:"Christopher Nolan",productionCompany:"Christopher Nolan",country:"America",year:2024, alreadyWatched: true},
+    {name:"Avatar",director:"James Cameron",productionCompany:"warner bros",country:"America",year:2023, alreadyWatched: true},
+    {name:"Spiderman",director:"Sam Raimi",productionCompany:"marvel",country:"America",year:2024, alreadyWatched: false}
+
+  ]
 
 }
