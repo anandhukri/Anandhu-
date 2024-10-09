62130ef31361c9f78537627b9d8be3dcce57c66f
Anandhukrishnan Pulikkal Sreenivasan <w0852083@myscc.ca>
2024-10-09 15:50:45 -0700

    created a service

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -14,13 +14,13 @@ import {NgForOf} from "@angular/common";
   styleUrl: './movie-list.component.css'
 })
 export class MovieListComponent {
-  movie1: Movies [] = [ {name:"Barroz",director:"Mohenlal",productionCompany : "Ashirvad cinemas", country : "India", year: 2024, alreadyWatched: true },
-    {name:"ARM",director:"Jithin lal",productionCompany:"Magic Frames",country:"",year:2024, alreadyWatched: false},
-    {name:"GOATLIFE",director:"Blessy",productionCompany:"PJ filims",country:"",year:2024, alreadyWatched: true},
-    {name:"Oppenheimer",director:"Christopher Nolan",productionCompany:"Christopher Nolan",country:"America",year:2024, alreadyWatched: true},
-    {name:"Avatar",director:"James Cameron",productionCompany:"warner bros",country:"America",year:2023, alreadyWatched: true},
-    {name:"Spiderman",director:"Sam Raimi",productionCompany:"marvel",country:"America",year:2024, alreadyWatched: false}
-
-  ]
+  // movie1: Movies [] = [ {name:"Barroz",director:"Mohenlal",productionCompany : "Ashirvad cinemas", country : "India", year: 2024, alreadyWatched: true },
+  //   {name:"ARM",director:"Jithin lal",productionCompany:"Magic Frames",country:"",year:2024, alreadyWatched: false},
+  //   {name:"GOATLIFE",director:"Blessy",productionCompany:"PJ filims",country:"",year:2024, alreadyWatched: true},
+  //   {name:"Oppenheimer",director:"Christopher Nolan",productionCompany:"Christopher Nolan",country:"America",year:2024, alreadyWatched: true},
+  //   {name:"Avatar",director:"James Cameron",productionCompany:"warner bros",country:"America",year:2023, alreadyWatched: true},
+  //   {name:"Spiderman",director:"Sam Raimi",productionCompany:"marvel",country:"America",year:2024, alreadyWatched: false}
+  //
+  // ]
 
 }
