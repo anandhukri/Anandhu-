b609de7216e57ad4c749b8c2690803b862ec129d
Anandhukrishnan Pulikkal Sreenivasan <w0852083@myscc.ca>
2024-10-09 17:33:44 -0700

    done till 8

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Movies} from "../Shared/Models/movies";
 import {MovieListItemComponent} from "../movie-list-item/movie-list-item.component";
 import {NgForOf} from "@angular/common";
+import {movie1} from "../data/mock-content";
+import {MovieService} from "../services/movie.service";
 
 @Component({
   selector: 'app-movie-list',
@@ -13,7 +15,7 @@ import {NgForOf} from "@angular/common";
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.css'
 })
-export class MovieListComponent {
+export class MovieListComponent implements OnInit{
   // movie1: Movies [] = [ {name:"Barroz",director:"Mohenlal",productionCompany : "Ashirvad cinemas", country : "India", year: 2024, alreadyWatched: true },
   //   {name:"ARM",director:"Jithin lal",productionCompany:"Magic Frames",country:"",year:2024, alreadyWatched: false},
   //   {name:"GOATLIFE",director:"Blessy",productionCompany:"PJ filims",country:"",year:2024, alreadyWatched: true},
@@ -22,5 +24,22 @@ export class MovieListComponent {
   //   {name:"Spiderman",director:"Sam Raimi",productionCompany:"marvel",country:"America",year:2024, alreadyWatched: false}
   //
   // ]
+  movieList: Movies[] = [];
 
+  constructor(private movieService: MovieService) {
+
+  }
+
+  ngOnInit() {
+    this.movieService.getMovies().subscribe({
+      next: (data: Movies[]) => this.movieList = data,
+      error:err => console.error("Error fetching Movies", err),
+      complete:() =>console.log("Movies data fetch complete!")
+
+
+    })
+  }
+
+
+  protected readonly movie1 = movie1;
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+//import our mock data
+import {movie1} from "../data/mock-content";
+import { Observable, of } from 'rxjs';
+import{Movies} from "../Shared/Models/movies";
 
+//Notice the new Decorator
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-
+  private movie: Movies[] = movie1;//Local copy of student data for CRUD Operations
   constructor() { }
-}
+  //Returns all students
+  getMovies(): Observable<Movies[]> {
+    return of(movie1); //Return and observable that emits mock student data
+  }}
+
