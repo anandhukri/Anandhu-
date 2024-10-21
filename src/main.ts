3bb5553ef3e8fb9429b7bce2a13875a253395bd8
Anandhukrishnan <w0852083@myscc.ca>
2024-10-21 11:04:36 -0700

    did till question 9

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import {provideRouter, Routes} from "@angular/router";
 //import {routes} from "./app/app.routes";
 import {MovieListComponent} from "./app/movie-list/movie-list.component";
 import {MovieListItemComponent} from "./app/movie-list-item/movie-list-item.component";
+import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
+import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
 
 
 
@@ -12,8 +14,8 @@ const routes: Routes =[
   {path: '',redirectTo: '/', pathMatch: 'full',},
   {path: 'movies',component: MovieListComponent},
   {path: 'movies/:name', component: MovieListItemComponent},
-  //{path: 'modify-movie', component: Modi},
-  {path: 'movies/:name', component: MovieListItemComponent},
+  {path: 'modify-movie', component: ModifyListItemComponent},
+  {path: 'nothing', component: PageNotFoundComponent},
 
 ]
 
