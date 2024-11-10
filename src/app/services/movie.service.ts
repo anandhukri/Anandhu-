03af53f619111ce11125dbc06608950d63adca1e
Anandhukrishnan <w0852083@myscc.ca>
2024-11-09 20:40:01 -0800

    did till 7

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,3 +1,57 @@
+// //
+// // import { Injectable } from '@angular/core';
+// // import { Observable, of } from 'rxjs';
+// // import { movie1 } from '../data/mock-content';
+// // import { Movies } from '../Shared/Models/movies';
+// //
+// // @Injectable({
+// //   providedIn: 'root'
+// // })
+// // export class MovieService {
+// //   private movies: Movies[] = movie1;
+// //
+// //   constructor() { }
+// //
+// //
+// //   getMovies(): Observable<Movies[]> {
+// //     return of(this.movies);
+// //   }
+// //
+// //   getMovieById(id: number): Observable<Movies | undefined> {
+// //     const movie = this.movies.find(movie => movie.id === id);
+// //     return of(movie);
+// //   }
+// //
+// //   addMovie(newMovie: Movies): Observable<Movies[]> {
+// //     this.movies.push(newMovie);
+// //     return of(this.movies);
+// //   }
+// //
+// //
+// //
+// //   updateMovie(updatedMovie: Movies): Observable<Movies | undefined> {
+// //     const index = this.movies.findIndex(movie => movie.id === updatedMovie.id);
+// //     if (index !== -1) {
+// //       this.movies[index] = updatedMovie;
+// //       console.log('Updated movie:', this.movies[index]);
+// //       return of(this.movies[index]); // Return only the updated movie
+// //     } else {
+// //       console.log('Movie not found with ID:', updatedMovie.id);
+// //       return of(undefined);
+// //     }
+// //   }
+// //
+// //
+// //
+// //
+// //   deleteMovie(movieId: number): Observable<Movies[]> {
+// //     this.movies = this.movies.filter(movie => movie.id !== movieId);
+// //     return of(this.movies);
+// //   }
+// // }
+// //
+// //
+// //
 //
 // import { Injectable } from '@angular/core';
 // import { Observable, of } from 'rxjs';
@@ -11,7 +65,6 @@
 //   private movies: Movies[] = movie1;
 //
 //   constructor() { }
-//
 //
 //   getMovies(): Observable<Movies[]> {
 //     return of(this.movies);
@@ -27,22 +80,16 @@
 //     return of(this.movies);
 //   }
 //
-//
-//
-//   updateMovie(updatedMovie: Movies): Observable<Movies | undefined> {
+//   updateMovie(updatedMovie: Movies): Observable<Movies[]> {
 //     const index = this.movies.findIndex(movie => movie.id === updatedMovie.id);
 //     if (index !== -1) {
 //       this.movies[index] = updatedMovie;
 //       console.log('Updated movie:', this.movies[index]);
-//       return of(this.movies[index]); // Return only the updated movie
 //     } else {
 //       console.log('Movie not found with ID:', updatedMovie.id);
-//       return of(undefined);
 //     }
+//     return of(this.movies); // Return the updated list
 //   }
-//
-//
-//
 //
 //   deleteMovie(movieId: number): Observable<Movies[]> {
 //     this.movies = this.movies.filter(movie => movie.id !== movieId);
@@ -51,51 +98,67 @@
 // }
 //
 //
-//
-
 
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { movie1 } from '../data/mock-content';
+import { catchError } from 'rxjs/operators';
 import { Movies } from '../Shared/Models/movies';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-  private movies: Movies[] = movie1;
+  private moviesUrl = 'api/movies'; // URL to the in-memory API
 
-  constructor() { }
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
+  constructor(private http: HttpClient) { }
+
+  // Fetch all movies
   getMovies(): Observable<Movies[]> {
-    return of(this.movies);
+    return this.http.get<Movies[]>(this.moviesUrl).pipe(
+      catchError(this.handleError<Movies[]>('getMovies', []))
+    );
   }
 
+  // Fetch a movie by ID
   getMovieById(id: number): Observable<Movies | undefined> {
-    const movie = this.movies.find(movie => movie.id === id);
-    return of(movie);
+    const url = `${this.moviesUrl}/${id}`;
+    return this.http.get<Movies>(url).pipe(
+      catchError(this.handleError<Movies>(`getMovieById id=${id}`))
+    );
   }
 
-  addMovie(newMovie: Movies): Observable<Movies[]> {
-    this.movies.push(newMovie);
-    return of(this.movies);
+  // Add a new movie
+  addMovie(newMovie: Movies): Observable<Movies> {
+    return this.http.post<Movies>(this.moviesUrl, newMovie, this.httpOptions).pipe(
+      catchError(this.handleError<Movies>('addMovie'))
+    );
   }
 
-  updateMovie(updatedMovie: Movies): Observable<Movies[]> {
-    const index = this.movies.findIndex(movie => movie.id === updatedMovie.id);
-    if (index !== -1) {
-      this.movies[index] = updatedMovie;
-      console.log('Updated movie:', this.movies[index]);
-    } else {
-      console.log('Movie not found with ID:', updatedMovie.id);
-    }
-    return of(this.movies); // Return the updated list
+  // Update an existing movie
+  updateMovie(updatedMovie: Movies): Observable<any> {
+    return this.http.put(this.moviesUrl, updatedMovie, this.httpOptions).pipe(
+      catchError(this.handleError<any>('updateMovie'))
+    );
   }
 
-  deleteMovie(movieId: number): Observable<Movies[]> {
-    this.movies = this.movies.filter(movie => movie.id !== movieId);
-    return of(this.movies);
+  // Delete a movie by ID
+  deleteMovie(movieId: number): Observable<Movies> {
+    const url = `${this.moviesUrl}/${movieId}`;
+    return this.http.delete<Movies>(url, this.httpOptions).pipe(
+      catchError(this.handleError<Movies>('deleteMovie'))
+    );
+  }
+
+  // Handle errors
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
   }
 }
-
-
--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,85 @@
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { AppComponent } from './app/app.component';
+// import {provideRouter, RouterModule, Routes} from "@angular/router";
+// //import {routes} from "./app/app.routes";
+// import {MovieListComponent} from "./app/movie-list/movie-list.component";
+// import {MovieListItemComponent} from "./app/movie-list-item/movie-list-item.component";
+// import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
+// import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
+// import {NgModule} from "@angular/core";
+//
+//
+//
+//
+// const routes: Routes =[
+//   {path: '',redirectTo: '/', pathMatch: 'full',},
+//   {path: 'movies',component: MovieListComponent},
+//   {path: 'movies/:name', component: MovieListItemComponent},
+//   {path: 'modify-list-item/id', component: MovieListItemComponent},
+//   {path: 'modify-movie', component: ModifyListItemComponent},
+//   {path: 'nothing', component: PageNotFoundComponent},
+//   { path: 'modify-list-item/:id', component: ModifyListItemComponent }
+//
+//
+// ];
+// @NgModule({imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]})
+//
+// export class AppRoutingModule {}
+//
+// bootstrapApplication(AppComponent, {
+//   providers: [provideRouter(routes)]
+// })
+//   .catch((err) => console.error(err));
+//
+//
+//
+
+
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import {provideRouter, RouterModule, Routes} from "@angular/router";
-//import {routes} from "./app/app.routes";
-import {MovieListComponent} from "./app/movie-list/movie-list.component";
-import {MovieListItemComponent} from "./app/movie-list-item/movie-list-item.component";
-import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
-import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
-import {NgModule} from "@angular/core";
+import { provideRouter, RouterModule, Routes } from '@angular/router';
+import {HttpClientModule, provideHttpClient} from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './app/services/in-memory-data.service'; // Ensure this path is correct
+import {importProvidersFrom, NgModule} from '@angular/core';
 
+// Components
+import { MovieListComponent } from './app/movie-list/movie-list.component';
+import { MovieListItemComponent } from './app/movie-list-item/movie-list-item.component';
+import { ModifyListItemComponent } from './app/modify-list-item/modify-list-item.component';
+import { PageNotFoundComponent } from './app/page-not-found/page-not-found.component';
 
-
-
-const routes: Routes =[
-  {path: '',redirectTo: '/', pathMatch: 'full',},
-  {path: 'movies',component: MovieListComponent},
-  {path: 'movies/:name', component: MovieListItemComponent},
-  {path: 'modify-list-item/id', component: MovieListItemComponent},
-  {path: 'modify-movie', component: ModifyListItemComponent},
-  {path: 'nothing', component: PageNotFoundComponent},
-  { path: 'modify-list-item/:id', component: ModifyListItemComponent }
-
-
+// Routes
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'movies', component: MovieListComponent },
+  { path: 'movies/:name', component: MovieListItemComponent },
+  { path: 'modify-list-item/:id', component: ModifyListItemComponent },
+  { path: 'modify-movie', component: ModifyListItemComponent },
+  { path: 'nothing', component: PageNotFoundComponent }
 ];
-@NgModule({imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]})
 
+// NgModule configuration for routing and HTTP in-memory API
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false })
+  ],
+  exports: [RouterModule]
+})
 export class AppRoutingModule {}
 
+// Bootstrap the application
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
-})
-  .catch((err) => console.error(err));
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+    importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 1000 })) // Import providers dynamically
 
-
+  ]
+}).catch((err) => console.error(err));
 
 
